94e9e415bddc61055f320f1150df6c9fb63f29dd
radinmorik <91218315+radinmorik@users.noreply.github.com>
2025-02-06 15:46:12 +0100

    Add View Package and initialize JFrame window

--- ExamScheduling/src/main/java/no/hiof/examplanner/Main.java
+++ ExamScheduling/src/main/java/no/hiof/examplanner/Main.java
@@ -1,13 +1,18 @@
 package no.hiof.examplanner;
 
+import no.hiof.examplanner.view.MainFrame;
+
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
 
-        System.out.println("Test");
+            frame.setVisible(true);
+        });
     }
-
-
 
 }
--- ExamScheduling/src/main/java/no/hiof/examplanner/view/MainFrame.java
+++ ExamScheduling/src/main/java/no/hiof/examplanner/view/MainFrame.java
@@ -1,4 +1,22 @@
 package no.hiof.examplanner.view;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame{
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    public MainFrame() {
+        setTitle("Smart Exam Planner");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+    }
+
 }
